4fd4296617f7cd40e0d75fe89c9c665c49837ce7
SUJITH C S <43125517+Code-Suji@users.noreply.github.com>
2021-09-15 10:44:40 +0530

    Update PersistenceConfiguration.java

--- src/main/java/com/suji/cms/config/PersistenceConfiguration.java
+++ src/main/java/com/suji/cms/config/PersistenceConfiguration.java
@@ -7,14 +7,14 @@ import org.springframework.context.annotation.Configuration;
 import javax.sql.DataSource;
 @Configuration
 public class PersistenceConfiguration {
-    @Bean
-    public DataSource dataSource() {
-        DataSourceBuilder builder = DataSourceBuilder.create();
-        builder.url("jdbc:postgresql://localhost:5432/conference_app");
-        builder.username("postgres");
-        builder.password("password");
+//     @Bean
+//     public DataSource dataSource() {
+//         DataSourceBuilder builder = DataSourceBuilder.create();
+//         builder.url("jdbc:postgresql://localhost:5432/conference_app");
+//         builder.username("postgres");
+//         builder.password("password");
 
-        System.out.println("My custom datasource bean has been initialized and set");
-        return builder.build();
-    }
+//         System.out.println("My custom datasource bean has been initialized and set");
+//         return builder.build();
+//     }
 }
